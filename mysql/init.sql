b032e47c38bc746209bcb7a6ef177fa1a762ec87
akshayvv02 <akvv2596@gmail.com>
2025-04-21 12:10:42 -0400

    Create init.sql

---
+++ mysql/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS demodb;
+
+USE demodb;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES
+('Akshay', 'akshay@example.com'),
+('John Doe', 'john@example.com');
